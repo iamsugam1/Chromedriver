c3b489c516b4a85a39d5687fa99034112c246347
iamsugam1 <iamsugam@gmail.com>
2020-12-11 07:58:10 -0600

    "Getting Friends List"

--- QA Automation/src/Seleniumbasics/AutomationFacebook.java
+++ QA Automation/src/Seleniumbasics/AutomationFacebook.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-
 public class AutomationFacebook {
 
 	public static void main(String[] args) {
@@ -19,15 +18,14 @@ public class AutomationFacebook {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-notifications");
 		WebDriver driver = new ChromeDriver(options);
-		
-	
+
 		driver.get("https://www.facebook.com");
 //		System.out.println(driver.getTitle());
 //		System.out.println(driver.getCurrentUrl());
 		driver.findElement(By.id("email")).sendKeys("anticbaby@gmail.com");
 		driver.findElement(By.id("pass")).sendKeys("qaclass123");
 		driver.findElement(By.name("login")).click();
-		
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.findElement(By.xpath(
@@ -36,14 +34,12 @@ public class AutomationFacebook {
 		driver.findElement(By.xpath("//a[text()='Friends']")).click();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		List<WebElement> Allfriends = driver.findElements(By.xpath("//div[@class='buofh1pr hv4rvrfc']/div/a"));
-	
+
 		System.out.println(Allfriends.size());
-		for(int i=0; i<Allfriends.size(); i++) {
+		for (int i = 0; i < Allfriends.size(); i++) {
 			System.out.println(Allfriends.get(i).getText());
 		}
-	
-	
+		driver.close();
+
 	}
 }
-
-
